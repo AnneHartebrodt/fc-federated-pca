e70ff89779edc3928aaf6c9d96a3c9c4442b24d4
Anne Hartebrodt <anne.hartebrodt@tum.de>
2021-11-23 18:18:30 +0100

    modifiy configs

--- app/test/generate_report.sh
+++ app/test/generate_report.sh
@@ -70,7 +70,7 @@ tests=$(printf "$basedir/tests/%s/$cl/logs.txt " "${od[@]}")
 ids=$(printf "%s " "${od[@]}")
 echo $tests
 echo $ids
-python $pydir/runstats.py -d $test_report -o "run_summaries.tsv" -f $tests -i $ids
+#python $pydir/runstats.py -d $test_report -o "run_summaries.tsv" -f $tests -i $ids
 # generate report
 python $pydir/generate_report.py -d $test_report/test_results -r $test_report/report.md
 pandoc $test_report/report.md -f markdown -t html -o $test_report/report.html --css $pydir/templates/pandoc.css
