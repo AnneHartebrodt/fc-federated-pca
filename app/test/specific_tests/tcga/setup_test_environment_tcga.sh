f4f4dfd294cc44aa09a3874b81c64a9d59a26b73
Anne Hartebrodt <anne.hartebrodt@tum.de>
2021-11-25 17:55:07 +0100

    add files

--- app/test/specific_tests/tcga/setup_test_environment_tcga.sh
+++ app/test/specific_tests/tcga/setup_test_environment_tcga.sh
@@ -30,7 +30,9 @@ dirs=$(printf "%s " "${dirs[@]}")
 #compute canonical solution
 echo $dirs
 mkdir -p $outputfolder/$ct
-python $pydir/compute_canonical_solution.py -d $outputfolder/$ct -L $dirs -k $k -s $seed --transpose True --rownames 0
+mkdir -p $outputfolder/$ct/data
+cp -r $datafolder/$ct $outputfolder/$ct/data
+#python $pydir/compute_canonical_solution.py -d $outputfolder/$ct -L $dirs -k $k -s $seed --transpose True --rownames 0
 
 #split the data into batches
 batch=False
